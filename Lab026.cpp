46912630050e6e884cb66bcb7d2f49b4e77fc6cd
Bagabandi152 <76275588+Bagabandi152@users.noreply.github.com>
2021-09-29 21:55:55 +0800

    Update Lab026.cpp
    1

--- Lab026.cpp
+++ Lab026.cpp
@@ -9,20 +9,13 @@ void swapped(int *x, int *y){
 }
 
 int main(){
-
     int a, b;
     cout<<"Enter value of a and b: ";
     cin>>a>>b;
-
     cout<<"Solihoos umnuh utguud "<<endl;
     cout<<"a - iin utga: "<<a<<endl<<"b - iin utga: "<<b<<endl;
-
     swapped(&a, &b);
-
     cout<<"Solisonii daraah utguud "<<endl;
     cout<<"a - iin utga: "<<a<<endl<<"b - iin utga: "<<b;
-
     return 0;
 }
-
-
